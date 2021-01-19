6fe4f1e6922382c7874cf049d89a3204357c0805
frank john <32237920+frank2ny@users.noreply.github.com>
2021-01-19 16:59:07 +0300

    version two of the project
    this version solved the issue of the sensors being overlapped one another causing one sensor no to work pefectly

--- handwash.ino
+++ handwash.ino
@@ -1,75 +1,60 @@
-/*all projects available
- * Auto handwash system using arduino
- * All code available in https://github.com/NirmalKnock/Auto_Hand_wash_system
 
- */
-int pump =8;
-int pump1 =9;
+#define trigPin1 A0
+#define echoPin1 A1
+#define trigPin2 A2
+#define echoPin2 A3
+int soapled=10;
+int waterled=11;
+int ALARM = 8;
+int pump = 9;
+long duration, distance, FIRSTSensor,SECONDSensor;
+void setup()
+{
 
-//sensor for water pump
-const int trigPin = 7;
-const int echoPin = 6;
+Serial.begin (9600);
+pinMode(soapled,OUTPUT);
+pinMode(waterled,OUTPUT);
+pinMode(trigPin1, OUTPUT);
+pinMode(echoPin1, INPUT);
+pinMode(trigPin2, OUTPUT);
+pinMode(echoPin2, INPUT);
 
-//sensor for soup solution
-const int trigPin1 = 4;
-const int echoPin1 = 5;
-
-float duration, distance,duration1, distance1;
-
-
-void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(pump,OUTPUT);
-  digitalWrite(pump,LOW);
-  pinMode(echoPin, INPUT);
-
-   pinMode(trigPin1, OUTPUT);
-  pinMode(pump1,OUTPUT);
-  digitalWrite(pump1,LOW);
-  pinMode(echoPin1, INPUT);
-  Serial.begin(9600);
+pinMode(ALARM, OUTPUT);pinMode(pump,OUTPUT);
+digitalWrite(ALARM, LOW);digitalWrite(pump,LOW);
  
 }
 
-void loop() {
-  digitalWrite(trigPin, LOW);
-  digitalWrite(trigPin1, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  digitalWrite(trigPin1, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-   digitalWrite(trigPin1, LOW);
-
-  duration = pulseIn(echoPin, HIGH);
-    duration1 = pulseIn(echoPin1, HIGH);
-
-  distance1 = (duration1*.0343)/2;
-    distance = (duration*.0343)/2;
-
-  Serial.print("Water Distance:  ");
-  Serial.println(distance);
-  Serial.print("Soap Distance:  ");
-  Serial.println(distance1);
-  delay(500);
-if(distance<15){    //use can increase the distancen   
-  /* Due to the TTL logic of relay and Arduino , I writen  digitalwrite as LOW, if after building your project ,if they works in opposite mode
-  just change digitalWrite as Hign in (if condition ) and Low in else condition.
-  Otherwise skip this comment*/
-    digitalWrite(pump,HIGH);
-    delay(7000);   //You can increase the water timing
-    digitalWrite(pump,LOW);
-  }
-  if(distance1<10){    //use can increase the distancen   
-  /* Due to the TTL logic of relay and Arduino , I writen  digitalwrite as LOW, if after building your project ,if they works in opposite mode
-  just change digitalWrite as Hign in (if condition ) and Low in else condition.
-  Otherwise skip this comment*/
-    digitalWrite(pump1,HIGH);
-    delay(3000);   //You can increase the water timing
-    digitalWrite(pump1,LOW);
-  }
-  else{
-    digitalWrite(pump,LOW);
-    digitalWrite(pump1,LOW);
-  }
+void loop() 
+{
+/////////////////////////////////////////////////////// 
+SonarSensor(trigPin1, echoPin1);
+FIRSTSensor = distance;
+SonarSensor(trigPin2, echoPin2);
+SECONDSensor = distance;
+digitalWrite(ALARM, LOW);
+digitalWrite(pump,LOW);
+Serial.print("S1:");Serial.println(FIRSTSensor); delayMicroseconds(10);
+Serial.print("S2:");Serial.println(SECONDSensor);delayMicroseconds(10);
+delay(1000);
+if((FIRSTSensor >= 5) & (FIRSTSensor <= 12)) 
+{digitalWrite(waterled, HIGH);
+  digitalWrite(ALARM, HIGH);
+delay(5500);
+Serial.println("water pump is on");
+digitalWrite(ALARM,LOW);digitalWrite(waterled,LOW);}
+if((SECONDSensor >= 5) & (SECONDSensor <= 12)) 
+{digitalWrite(soapled,HIGH);digitalWrite(pump, HIGH);
+Serial.println("Soap pump is on");
+delay(2000);
+digitalWrite(pump, LOW);digitalWrite(soapled,LOW);}
+}
+void SonarSensor(int trigPin,int echoPin)
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance = (duration/2) / 29.1;
 }
